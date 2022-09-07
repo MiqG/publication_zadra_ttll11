40d2faa0cc8afa4f7203456a2536542cb9b6f6b6
MiqG <miquelangladagirotto@gmail.com>
2022-09-07 15:55:09 +0200

    added plots for TTLL13 and aneuploidy

--- scripts/figures_aneuploidy_correlation.R
+++ scripts/figures_aneuploidy_correlation.R
@@ -16,7 +16,6 @@ require(gtools)
 require(writexl)
 require(latex2exp)
 
-GENE_OI = 'TTLL11'
 ORDER_OI = c('LUSC','UCEC','BRCA','STAD','LUAD','KIRP','THCA','KICH','COAD','LIHC','HNSC','PRAD','KIRC')
 
 # variables
@@ -36,28 +35,39 @@ N_SAMPLE = 1000
 correlations_aneuploidy_file = file.path(RESULTS_DIR,'files','correlation-genexpr_aneuploidy.tsv') 
 
 # outputs
-output_file = file.path(RESULTS_DIR,'figures','aneuploidy_correlations.pdf')
 output_figdata = file.path(RESULTS_DIR,'files','figdata-aneuploidy_correlations.xlsx')
 
 ##### FUNCTIONS ######
-make_plot = function(df, plt_title, gene_oi=GENE_OI, y_lab_pos=-0.6){
+stars_pval = function(p.value){
+    unclass(
+        symnum(
+            p.value, 
+            corr = FALSE, 
+            na = FALSE, 
+            cutpoints = c(0, 1e-04, 0.001, 0.01, 0.05, 1), 
+            symbols = c("****", "***", "**", "*", "ns")
+        )
+    )
+}
+
+make_plot = function(df, plt_title, gene_oi, y_lab_pos=-0.6){
     # plot spearman correlation values as stripchart and violin
     # highlighting TTLL11 and adding the p-value of the Z-score of 
     # TTLL11 as labels
     
     ## prepare pvalues in gene OI
-    stat.test = df %>% filter(gene == GENE_OI) %>% 
+    stat.test = df %>% filter(gene == gene_oi) %>% 
         mutate(
             p=pvalue, 
             p.format=format.pval(pvalue,1),
-            p.signif=stars.pval(pvalue),
+            p.signif=stars_pval(pvalue),
             group1=NA,
             group2=NA)
 
     ## plot
     plt = ggviolin(df, x='cancer_type', y='value', color='darkgrey', alpha=0.5, lwd=0.2) +
     geom_boxplot(width=0.1, outlier.shape = NA, lwd=0.1) + 
-    geom_point(data = df %>% filter(gene == 'TTLL11'), color='darkred', size=1) + 
+    geom_point(data = df %>% filter(gene == gene_oi), color='darkred', size=1) + 
     xlab(element_blank()) + ylab(element_blank()) + ggtitle(element_blank()) + 
     stat_pvalue_manual(stat.test, x = 'cancer_type', y.position = y_lab_pos, label = 'p.signif')
     plt = ggpar(plt, font.tickslab = c(BASE_SIZE))
@@ -97,8 +107,8 @@ df = df[order(df$cancer_type),]
 # plot by cancer
 df = df %>% drop_na()
 plt_title = 'Gene Expression and Aneuploidy Score'
-gene_oi = GENE_OI
-plts[['aneuploidy_bycancer']] = make_plot(df, plt_title, gene_oi, -0.7)
+plts[['aneuploidy_bycancer-TTLL11']] = make_plot(df, plt_title, "TTLL11", -0.7)
+plts[['aneuploidy_bycancer-TTLL13']] = make_plot(df, plt_title, "TTLL13", -0.7)
 
 # prepare pancancer
 df_pancan = df %>% 
@@ -109,17 +119,35 @@ df_pancan = df %>%
     mutate(cancer_type='PANCAN')
 
 # plot pancancer
-plts[['aneuploidy_pancancer']] = make_plot(df_pancan, plt_title, gene_oi, -0.4)
+plts[['aneuploidy_pancancer-TTLL11']] = make_plot(df_pancan, plt_title, "TTLL11", -0.4)
+plts[['aneuploidy_pancancer-TTLL13']] = make_plot(df_pancan, plt_title, "TTLL13", -0.4)
 
 # compose figure
 widths_cancer_type = c(1.15,0.13)
 
-plt = ggarrange(
-    plts[['aneuploidy_bycancer']] + 
+plts[["fig-TTLL11"]] = ggarrange(
+    plts[['aneuploidy_bycancer-TTLL11']] + 
         ylab('Spearman Correlation Coeff.') + 
         theme_pubr(base_size = FONT_SIZE) +
         theme(plot.margin = margin(0,0,0,0, "cm")),
-    plts[['aneuploidy_pancancer']] + 
+    plts[['aneuploidy_pancancer-TTLL11']] + 
+        theme_pubr(base_size = FONT_SIZE) +
+        theme(plot.margin = margin(0,0,0,0, "cm")),
+    widths = c(1.15,0.13),
+    common.legend = TRUE
+) %>% annotate_figure(bottom = text_grob('Cancer Type', 
+                                         size = FONT_SIZE, 
+                                         family = FONT_FAMILY,
+                                         vjust = -0.75)
+                     ) +
+    theme(plot.margin = margin(0.1,0.3,0.1,0.1, "cm"))
+
+plts[["fig-TTLL13"]] = ggarrange(
+    plts[['aneuploidy_bycancer-TTLL13']] + 
+        ylab('Spearman Correlation Coeff.') + 
+        theme_pubr(base_size = FONT_SIZE) +
+        theme(plot.margin = margin(0,0,0,0, "cm")),
+    plts[['aneuploidy_pancancer-TTLL13']] + 
         theme_pubr(base_size = FONT_SIZE) +
         theme(plot.margin = margin(0,0,0,0, "cm")),
     widths = c(1.15,0.13),
@@ -139,7 +167,10 @@ figdata = rbind(df[,common_cols], df_pancan[,common_cols]) %>%
 
 # save
 ## plots
-ggsave(plt, filename=output_file, units = 'cm', width=12, dpi=300, height=8, device=cairo_pdf)
+output_file = file.path(RESULTS_DIR,'figures','aneuploidy_correlations-TTLL11.pdf')
+ggsave(plts[["fig-TTLL11"]], filename=output_file, units = 'cm', width=12, dpi=300, height=8, device=cairo_pdf)
+output_file = file.path(RESULTS_DIR,'figures','aneuploidy_correlations-TTLL13.pdf')
+ggsave(plts[["fig-TTLL13"]], filename=output_file, units = 'cm', width=12, dpi=300, height=8, device=cairo_pdf)
 
 ## figure data
 write_xlsx(
